73402e38114a6057dbe182c53afe54fed33631ce
voidrak <raklovetocode@gmail.com>
2025-08-06 16:31:35 +0300

    fix: adjust layout structure in HeroSection for better content visibility

--- src/components/HomePage/HeroSection.vue
+++ src/components/HomePage/HeroSection.vue
@@ -5,24 +5,30 @@ import HeroImage from '@/../public/homepage_heroSection.webp'
 
 <template>
   <div
-    class="w-full h-[500px] md:h-[700px] text-white flex flex-col justify-center bg-cover bg-center px-4 md:px-0"
+    class="w-full h-[500px] md:h-[700px] text-white flex flex-col justify-center bg-cover bg-center px-4 md:px-0 relative"
     :style="`background-image: url('${HeroImage}')`"
   >
-    <div class="text-4xl md:text-8xl ml-4 md:ml-28 font-bold overflow-hidden">
-      <h1 class="animate-slide-up-1">Restoring Sight.</h1>
-      <h1 class="animate-slide-up-2">Restoring</h1>
-      <h1 class="animate-slide-up-3">Livelihoods.</h1>
+    <!-- Dark overlay -->
+    <div class="absolute inset-0 bg-black/40"></div>
+
+    <!-- Content with higher z-index -->
+    <div class="relative z-10">
+      <div class="text-4xl md:text-8xl ml-4 md:ml-28 font-bold overflow-hidden">
+        <h1 class="animate-slide-up-1">Restoring Sight.</h1>
+        <h1 class="animate-slide-up-2">Restoring</h1>
+        <h1 class="animate-slide-up-3">Livelihoods.</h1>
+      </div>
+      <p class="max-w-2xl text-base md:text-lg ml-4 md:ml-28 mt-4">
+        ClearSight Initiative provides affordable reading glasses to underserved communities across
+        Ethiopia empowering people to see clearly, work confidently, and live independently.
+      </p>
+      <RouterLink
+        :to="{ name: 'Donate' }"
+        class="px-6 md:px-8 w-fit py-2 ml-4 md:ml-28 flex items-center mt-6 md:mt-10 bg-accentGold pointer-cursor hover:bg-yellow-500 text-white rounded"
+      >
+        Donate Us
+      </RouterLink>
     </div>
-    <p class="max-w-2xl text-base md:text-lg ml-4 md:ml-28 mt-4">
-      ClearSight Initiative provides affordable reading glasses to underserved communities across
-      Ethiopia empowering people to see clearly, work confidently, and live independently.
-    </p>
-    <RouterLink
-      :to="{ name: 'Donate' }"
-      class="px-6 md:px-8 w-fit py-2 ml-4 md:ml-28 flex items-center mt-6 md:mt-10 bg-accentGold pointer-cursor hover:bg-yellow-500 text-white rounded"
-    >
-      Donate Us
-    </RouterLink>
   </div>
 </template>
 
